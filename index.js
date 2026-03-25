820596a1caf82bfff8728d420028e7cc29e346ef
abc <abc@.com>
2026-03-25 13:00:51 +0530

    map, filter , todo deletebtn ui

--- index.js
+++ index.js
@@ -1,77 +1,74 @@
-// let input = document.getElementById("input");
-// let list = document.getElementById("list");
-// let btn = document.getElementById("btn")
+let input = document.getElementById("input");
+let list = document.getElementById("list");
+let btn = document.getElementById("btn")
 
-// let data = []
-// data = JSON.parse(localStorage.getItem("myTask")) || [] ;
-// console.log(data.length);
+let data = []
+data = JSON.parse(localStorage.getItem("myTask")) || [] ;
+console.log(data.length);
 
-// function onLoad(data){
-//       if(data.length > 0){
-//         for(let i = 0 ; i < data.length ; i++){
-//           addTodo(data[i]);
-//         }
-//       }
-// }
+function onLoad(data){
+      if(data.length > 0){
+        data.forEach((task)=>{
+            addTodo(task);
+        })
+      }
+}
 
-// function addTodo(task){
-//         let li = document.createElement("li");
-//         li.innerText = task;
-//         list.appendChild(li);       
-//         input.value="";
-// }
+function addTodo(task){
+        let li = document.createElement("li");
+        let title = document.createElement("span")
+        let btnFunction = document.createElement("span")
+        let btnDelete = document.createElement("button")
+        btnDelete.innerText = "Delete";
 
-// input.addEventListener("keyup" ,function(event){ 
-//     if(event.keyCode == 13){
-//       data.push(input.value);
-//       localStorage.setItem("myTask" , JSON.stringify(data));
-//       addTodo(input.value)
-//     }
-// } )
+        btnFunction.appendChild(btnDelete);
 
-// btn.addEventListener("click",function(event){  
-//        addTodo(input.value)
-// })
+        title.innerText = task;
+        li.appendChild(title);
+        li.appendChild(btnFunction)    
+        list.appendChild(li)   
+        input.value="";
 
-// console.log("end")
+        //-----Delete Functionalty---
 
-// onLoad(data)
-
-
-// let obj1 = {
-//   a : 10,
-//   b : 20,
-//   c : {
-//     a: 50,
-//     b: 30
-//   }
-// }
-
-// //let obj2 = {...obj1};
-
-// let obj2 = JSON.parse(JSON.stringify(obj1))
-
-// obj2.c.b = 50;
+        btnDelete.addEventListener("click",function(e){
+              list.removeChild(li);
+        })
+}
 
 
-// console.log("obj2 = ",obj2)
-// console.log("obj1 = ",obj1)
 
-// Array Itrators
-
-
-// arr.ForEach()
-let arr = [1,2,3,4,5];
-
-// for(let i = 0 ; i < arr.length ; i++){
-//   console.log(arr[i]);
-// }
-
-arr.forEach(function(value , index , arr){
-    if(index%2 == 0){
-      console.log(value * 3 , index , value);
+input.addEventListener("keyup" ,function(event){ 
+    if(event.keyCode == 13){
+      data.push(input.value);
+      localStorage.setItem("myTask" , JSON.stringify(data));
+      addTodo(input.value)
     }
+} )
+
+btn.addEventListener("click",function(event){  
+       addTodo(input.value)
 })
 
+console.log("end")
 
+onLoad(data)
+
+//-----------------------------------------------------------
+
+// Array Itrators
+// arr.ForEach()
+// let arr = [1,2,3,4,5];
+// let arr2 ;
+
+// arr2 = arr.filter(function(value , index , array){
+//   if(index %2 == 0){
+//     return value ;
+
+//   }
+      
+// })
+
+// console.log(arr)
+// console.log(arr2)
 
